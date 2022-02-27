232fdcfafdf6be8f19f4a7e794ce0331d23c0035
thughari <thughari3@gmail.com>
2022-02-27 16:41:59 +0530

    first step

---
+++ app/src/main/java/com/example/myfirstapk/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myfirstapk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonClick(View view){
+        TextView var = findViewById(R.id.sampleText);
+        var.setText("hii");
+    }
+}
